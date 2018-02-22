61a3bbe625cca4c279574d693d242de3a8c1b89c
Kevin N. Andrews <me@kenygamer.com>
2018-02-22 20:46:37 -0300

    Update Main.php

--- src/plugin/Main.php
+++ src/plugin/Main.php
@@ -1,18 +1,17 @@
 <?php
 namespace plugin;
-# for what are the use statements?
+# No use statements.
 class Main extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener{
-        # var? in what age do you live? no visibility, bad practice storing Server instance
+        # var? no visibility, bad practice storing Server instance
         # in a class property when you could do PluginBase->getServer() or Server::getInstance()
         var $server = null;
-        # storing a boolean in a class property
+        # storing a boolean in a class property.
         var $noError = TRUE;
         var $hasError = FALSE;
         # no visibility for onEnable()
         function onEnable(){
                 # has no point, as explained above.
                 $this->server = \pocketmine\Server::getInstance();
-                $this->server->getPluginManager()->registerEvents($this, $this);
         }
         # avoid < 3-character variable names
         function onCommand(\pocketmine\command\CommandSender $s, \pocketmine\command\Command $c, string $l, array $a) : bool{
@@ -22,9 +21,10 @@ class Main extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Li
                         return $this->hasError;
                 }
                 # no check done if level is loaded... e.g === null
-                # Level->setBlock() will return error
+                # Level->setBlock() will return error if level is not loaded.
                 $l = $this->server->getLevelByName($a[3]);
                 $l->setBlockIdAt($a[0], $a[1], $a[2], $a[4]);
+                $s->sendMessage("Success");
                 return $this->noError;
         }
 }
